858f69e5a39bc2e3de05343941d51b1d5b144e64
cynthia shelly <you@example.com>
2022-09-06 19:57:32 -0400

    Add event listeners, form input

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-function validFormFieldInput(data);
+function validFormFieldInput(data) {
+    if (data == null || data == '') {
+       const warning = document.querySelector('#form-warning');
+        warning.style.display = "block"
+    }
+};
+const newTaskInput = document.querySelector('#assign');
+
+newTaskInput.addEventListener('submit', (eventE) => {
+    const taskNameInput = document.querySelector('#doThis');
+    const taskNameValue = taskNameInput.value;
+    const descriptionInput = document.querySelector('#describeIt');
+    const descriptionInputValue = descriptionInput.value;
+    const assignedToInput = document.querySelector('#forYou');
+    const assignedToValue = assignedToInput.value;
+validFormFieldInput(taskNameValue);
+    
+})
+
+
+
+
+
+//console.log("name:  "+name);
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,3 +1,7 @@
+//function createTaskHtml()
+//createTaskHtml(name, description, assignedTo, dueDate, status)
+
+
 class TaskManager {
     constructor(){ 
     this.tasks = [];
